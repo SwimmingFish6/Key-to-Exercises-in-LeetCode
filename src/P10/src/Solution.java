64f7d431ca2594c4b1ade271e2e6f8c8ddc7d432
SwimmingFish6 <luxuhui12345@126.com>
2018-01-30 00:38:46 -0800

    update p10

--- src/P10/src/Solution.java
+++ src/P10/src/Solution.java
@@ -9,7 +9,7 @@ class Solution {
         }
 
         for (int j = 1;j <= p.length();j++) {
-            dp[0][j] = dp[0][j - 1] && p.charAt(j - 1) == '*';
+            dp[0][j] = j > 1 && dp[0][j - 2] && p.charAt(j - 1) == '*';
         }
 
         for (int i = 1;i <= s.length();i++) {
@@ -18,8 +18,7 @@ class Solution {
                     dp[i][j] = dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');
                 }
                 else {
-                    if (j < 2) dp[i][j] = (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 1) == '.'));
-                    else dp[i][j] = (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')) || dp[i][j - 2] || dp[i][j - 1];
+                    dp[i][j] = (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')) || dp[i][j - 2];
                 }
             }
         }
